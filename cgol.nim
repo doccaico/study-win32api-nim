76b240019c76e502440e3f1b455bfdd7f6a36467
doccaico <hash_ext_luna@yahoo.co.jp>
2025-07-03 10:42:25 +0900

    up

--- cgol.nim
+++ cgol.nim
@@ -1,10 +1,11 @@
 import std/[random]
 import winim/lean
+import winim/inc/mmsystem
 
 const WindowTitle = when defined(release) or defined(danger): "lifegame" else: "lifegame (debug)"
 const CellSize = 2
 const InitialCellCount = 40 * 2
-# const Fps = 30 / 2
+const Fps = 30 div 2
 const ScreenWidth = 480
 const ScreenHeight = 640
 const ColSize = (ScreenWidth div CellSize + 2)
@@ -12,13 +13,6 @@ const RowSize = (ScreenHeight div CellSize + 2)
 const InitialCellColor = RGB(0, 0, 0)
 const InitialBgColor = RGB(245, 245, 245)
 
-# const InitialCellColor = RGB(50, 50, 50)
-# # const InitialBgColor = RGB(245, 245, 245)
-# const InitialBgColor = RGB(0, 0, 0)
-
-# const InitialCellColor = CreateSolidBrush(RGB(50, 50, 50))
-# const InitialBgColor = RGB(245, 245, 245)
-# const InitialBgColor = CreateSolidBrush(RGB(0, 0, 0))
 
 type
   State = enum
@@ -31,34 +25,42 @@ type
     boardNeighbors: array[RowSize, array[ColSize, uint8]]
     cellColor: HBRUSH
     bgColor: HBRUSH
+    cx: int32
+    cy: int32
 
-proc shuffleBoard(app: var App)
+var
+  app : App 
+  frames: int = 0
+  bitmap : HBITMAP 
+  screenDC : HDC
+  renderDC : HDC
 
-proc newGame(app: var App) =
+
+proc shuffleBoard() =
+  for i in 1..<RowSize - 1:
+    let minN = 1
+    let maxN = app.board[i].len - 2
+    var j = maxN - 1
+    while j > 0:
+      let n = rand(minN..maxN)
+      swap(app.board[i][j], app.board[i][n])
+      dec j
+
+proc newGame() =
   app.cellColor = CreateSolidBrush(InitialCellColor)
   app.bgColor = CreateSolidBrush(InitialBgColor) 
   for i in 1..<RowSize - 1:
+    zeroMem(addr app.board[i], (sizeof State) * app.board[i].len)
     for j in 1..<ColSize - 1:
-      app.board[i][j] = if 1 <= j and j <= InitialCellCount: alive else: dead
-  app.shuffleBoard()
+      if 1 <= j and j <= InitialCellCount:
+        app.board[i][j] =  alive
+  shuffleBoard()
 
-proc draw(app: App, buffer: HDC) =
-  SelectObject(buffer, app.cellColor)
-  for i in 1..<RowSize - 1:
-    for j in 1..<ColSize - 1:
-      if app.board[i][j] == alive:
-        PatBlt(buffer, int32(CellSize * (j - 1)), int32(CellSize * (i - 1)), CellSize, CellSize, PATCOPY)
+proc clearBackGround() =
+  SelectObject(renderDC, app.bgColor)
+  PatBlt(renderDC, 0, 0, app.cx, app.cy, PATCOPY)
 
-proc shuffleBoard(app: var App) =
-  for i in 1..<RowSize - 1:
-    shuffle(app.board[i])
-
-
-proc clear(app: App, buffer: HDC) =
-  SelectObject(buffer, app.bgColor)
-  PatBlt(buffer, 0, 0, ScreenWidth, ScreenHeight, PATCOPY)
-
-proc nextGeneration(app: var App) =
+proc nextGeneration() =
   for i in 1..<RowSize - 1:
     for j in 1..<ColSize - 1:
       # top = top-left + top-middle + top-right
@@ -80,52 +82,35 @@ proc nextGeneration(app: var App) =
       else:
         app.board[i][j] = dead
 
-proc setClientSize(hwnd: HWND, sx: int, sy: int) =
+proc render(hwnd: HWND) =
+  SelectObject(renderDC, app.cellColor)
+  for i in 1..<RowSize - 1:
+    for j in 1..<ColSize - 1:
+      if app.board[i][j] == alive:
+        PatBlt(renderDC, int32(CellSize * (j - 1)), int32(CellSize * (i - 1)), CellSize, CellSize, PATCOPY)
+  BitBlt(screenDC, 0, 0, ScreenWidth, ScreenHeight, renderDC, 0, 0, SRCCOPY)
+
+proc setClientSize(app: var App, hwnd: HWND, sx: int, sy: int) =
   var
     rc1: RECT
     rc2: RECT
   GetWindowRect(hwnd, &rc1)
   GetClientRect(hwnd, &rc2)
-  let cx = int32(sx + ((rc1.right - rc1.left) - (rc2.right - rc2.left)))
-  let cy = int32(sy + ((rc1.bottom - rc1.top) - (rc2.bottom - rc2.top)))
-  let x = GetSystemMetrics(SM_CXSCREEN) div 2 - cx div 2
-  let y = GetSystemMetrics(SM_CYSCREEN) div 2 - cy div 2
-  SetWindowPos(hwnd, 0, x, y, cx, cy, SWP_NOZORDER or SWP_NOOWNERZORDER)
+  app.cx = int32(sx + ((rc1.right - rc1.left) - (rc2.right - rc2.left)))
+  app.cy = int32(sy + ((rc1.bottom - rc1.top) - (rc2.bottom - rc2.top)))
 
 proc WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.} =
-  var
-    app {.global.}: App 
-    bitmap {.global.}: HBITMAP 
-    buffer {.global.}: HDC
-    brush {.global.}: HBRUSH
-
   case message
   of WM_CREATE:
-    app.newGame();
 
-    var hdc = GetDC(hwnd)
-    bitmap = CreateCompatibleBitmap(hdc, ScreenWidth, ScreenHeight)
-    buffer = CreateCompatibleDC(hdc)
-
-    SelectObject(buffer, bitmap)
-    SelectObject(buffer, app.cellColor)
-    ReleaseDC(hwnd, hdc);
+    app.setClientSize(hwnd, ScreenWidth, ScreenHeight)
+    let x = GetSystemMetrics(SM_CXSCREEN) div 2 - app.cx div 2
+    let y = GetSystemMetrics(SM_CYSCREEN) div 2 - app.cy div 2
+    SetWindowPos(hwnd, 0, x, y, app.cx, app.cy, SWP_NOZORDER or SWP_NOOWNERZORDER)
     return 0
-  of WM_PAINT:
-    var ps: PAINTSTRUCT
-    var hdc = BeginPaint(hwnd, addr ps)
-    defer: EndPaint(hwnd, addr ps)
-
-    app.clear(buffer)
-    app.draw(buffer)
-    app.nextGeneration()
-
-    BitBlt(hdc, 0, 0, ScreenWidth, ScreenHeight, buffer, 0, 0, SRCCOPY)
-    return 0
-
   of WM_DESTROY:
-    DeleteObject(brush)
-    DeleteDC(buffer);
+    DeleteDC(screenDC);
+    DeleteDC(renderDC);
     DeleteObject(bitmap);
     PostQuitMessage(0)
     return 0
@@ -135,6 +120,8 @@ proc WindowProc(hwnd: HWND, message: UINT, wParam: WPARAM, lParam: LPARAM): LRES
 
 proc main() =
   randomize()
+
+  newGame();
 
   var
     hInstance = GetModuleHandle(nil)
@@ -175,20 +162,44 @@ proc main() =
     nil)
 
   if hwnd != 0:
-    setClientSize(hwnd, ScreenWidth, ScreenHeight)
     ShowWindow(hwnd, SW_SHOW)
     UpdateWindow(hwnd)
+  else:
+    MessageBox(0, "Error CreateWindowEx failed", appName, MB_ICONERROR)
+    return
 
+  screenDC = GetDC(hwnd)
+  bitmap = CreateCompatibleBitmap(screenDC, ScreenWidth, ScreenHeight)
+  renderDC = CreateCompatibleDC(screenDC)
+
+  SelectObject(renderDC, bitmap)
+  SelectObject(renderDC, app.cellColor)
+
+  const TicksPerSecond = Fps
+  const SkipTicks = 1000 / TicksPerSecond
+  
+  timeBeginPeriod(1)
+  SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS)
+  var nextGameTick = timeGetTime()
+
+  # A Game loop using PeekMessage function
+  # https://gist.github.com/ChlorUpload/8c560f72cbd4222bdaff90a99db6ff40
   while true:
-    let ret = GetMessage(addr msg, 0, 0, 0)
-    if ret != 0:
-      if ret == -1:
-        MessageBox(0, "Error GetMessage failed", appName, MB_ICONERROR)
-        return
-      else:
-        TranslateMessage(addr msg)
-        DispatchMessage(addr msg)
+    if PeekMessage(addr msg, 0, 0, 0, PM_REMOVE): 
+        if msg.message == WM_QUIT:
+            break
+        TranslateMessage(&msg)
+        DispatchMessageW(&msg)
     else:
-      break
+      Sleep(1)
+      if timeGetTime() > nextGameTick:
+        inc frames
+        clearBackGround()
+        nextGeneration()
+        render(hwnd)
+        nextGameTick +=  DWORD(SkipTicks)
+
+  timeEndPeriod(1)
+  return
 
 main()
